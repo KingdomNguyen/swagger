08765e9d2628d22e87b576ddad1ad121bfce5097
KingdomNguyen_Steven <106288384+KingdomNguyen@users.noreply.github.com>
2023-07-17 18:39:00 +0700

    Add files via upload

--- models/todo.js
+++ models/todo.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const TodoSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    task: {type: String, required: true},
+    completed: {type: Boolean, required: true, default: false},
+    canView: {type: Array, required: true},
+    canChange: {type: Array, required: true}
+})
+
+module.exports = mongoose.model("Todo",TodoSchema)
